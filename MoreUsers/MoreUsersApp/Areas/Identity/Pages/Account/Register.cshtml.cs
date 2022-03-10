b866f2bb480280ed537be009cd14569b095c0808
dankata7 <d.vladimirov2017@pmg-pernik.com>
2022-03-10 14:05:10 +0200

    client-all-ok

--- MoreUsers/MoreUsersApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MoreUsers/MoreUsersApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,7 +82,10 @@ namespace MoreUsersApp.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user,
+                   "Client"). Wait();
                     _logger.LogInformation("User created a new account with password.");
+                  
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
